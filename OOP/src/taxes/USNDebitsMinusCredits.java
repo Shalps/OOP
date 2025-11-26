26ff3e63904de19f011387f4d1947baa97d1fea8
Shalps <Chirikkirill97@gmail.com>
2025-11-26 17:27:40 +0300

    Delete OOP directory

--- OOP/src/taxes/USNDebitsMinusCredits.java
+++
@@ -1,8 +0,0 @@
-package taxes;
-
-public class USNDebitsMinusCredits extends TaxSystem {
-    @Override
-    public int calcTaxFor(int debit, int credit) {
-        return Math.max((debit - credit)/100*15,0);
-    }
-}
